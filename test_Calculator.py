17bbd4ed4b18a80ed78818eaf6f6871d0529ab47
Administrator <859403796@qq.com>
2021-04-12 22:59:31 +0800

    第二次作业

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,36 +1,30 @@
-
 import pytest
+import yaml
 
-from Calculator import Calculator
-
+def getdatas():
+    with open("./datas.yaml") as f:
+        datas = yaml.safe_load(f)
+        return datas
 
 class Testcal:
 
-    def setup_class(self):
-        print("开始计算")
-        self.calc = Calculator()
+    # def setup_class(self):
+    #     print("开始计算")
+    #     self.calc = Calculator()
+    #
+    # def teardown_class(self):
+    #     print("计算结束")
 
-    def teardown_class(self):
-        print("计算结束")
+    # @pytest.mark.parametrize("a,b,expect",getdatas()["int_datas"],ids=getdatas()["ids"])
+    @pytest.mark.last
+    def test_add(self,initcal,getint):
+        assert getint[2] == round(initcal.add(getint[0],getint[1]),2)
 
-    @pytest.mark.parametrize("a,b,expect",[
-        (1,1,2),(0.2,0.2,0.4),(1.1,1.1,2.2),(0,22,22)
-    ])
-    def test_add(self,a,b,expect):
-        assert expect == self.calc.add(a,b)
-    @pytest.mark.parametrize("a,b,expect",[
-        [1,2,0.5],[10,0.5,20],[0,2,0],[10,0,"除数不能为零"]
-    ])
-    def test_div(self,a,b,expect):
-        assert expect == self.calc.div(a,b)
-    @pytest.mark.parametrize("a,b,expect",[
-        [1,1,1],[0.1,2,0.2],[0,10,0],[10,10,100]
-    ])
-    def test_mul(self,a,b,expect):
-        assert expect == self.calc.mul(a,b)
 
-    @pytest.mark.parametrize("a,b,expect", [
-        [1,1,0], [0,2,-2], [0.2,0.1,0.1], [10,0.1,9.9]
-    ])
-    def test_sub(self, a, b, expect):
-        assert expect == self.calc.sub(a, b)
+    # @pytest.mark.parametrize("a,b,expect",getdatas()["div_datas"],ids=getdatas()["ids"])
+    @pytest.mark.first
+    def test_div(self,initcal,getdiv):
+        try:
+            assert getdiv[2] == initcal.div(getdiv[0],getdiv[1])
+        except ZeroDivisionError:
+            print("除数为零")
